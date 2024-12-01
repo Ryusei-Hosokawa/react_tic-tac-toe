90b9ff0898b9eae4b33f14da2d08e92b1df34bcc
Ryusei-Hosokawa <korokoro.seika@gmail.com>
2024-12-01 12:39:40 +0900

    12/1 second commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import Game from "./Game";
 
 export default function App() {
+    (function bodyAddClass() {
+        document.body.classList.add('bg-[#e5e5e5]');
+    })();
+    
     return (
         <>
-            <div className="bg-[#e5e5e5] h-[100vh] sm:grid sm:place-items-center sm:p-0 pt-[10vw] flex">
+            <div className="h-[100vh] sm:grid sm:place-items-center sm:p-0 pt-[10vw] flex">
                 <Game />
             </div>
         </>
